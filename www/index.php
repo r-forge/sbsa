412d044495ec31fe8a852f52973a7f62feba40b7
davorc (Davor Cubranic) <cubranic@stat.ubc.ca>
2012-01-14 01:19:16 +0000

    Replace the placeholder web page with real content
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sbsa@3 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,9 +43,54 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
+<p>
+  SBSA is an R package that offers a simplified interface to Bayesian
+  sensitivity analysis. It allows a formal but natural approach to
+  investigation of the nature of poorly measured and unobserved
+  confounders in collected data.
+</p>
 
-<p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
+<p>
+  Consider a study investigating the association between an outcome
+  and a set of exposure variables. Some of the variables are
+  confounders, sometimes poorly measured. Other potential confounders
+  may have gone unmeasured. Therefore, simply using regression on this
+  data brings a while range of potential problems.
+</p>
+
+<p>
+  <a href="http://en.wikipedia.org/wiki/Sensitivity_analysis">Sensitivity
+  analysis</a> can be used to asses how these limitations in available
+  data impact inference. In practice, however, sensitivity analysis
+  can be challenging to perform, or to interpret
+  results. <em>Simplified Bayesian sensitivity analysis</em>, proposed
+  by Gustafson <em>et al.</em><a href="#Gustafson2009">[1]</a>, aims to
+  find a blance between realism and simplicity in conducting
+  sensitivity analysis. It provides a model of data that requires the
+  user to provide only a few hyperparameters, and a computational
+  method to effectively apply MCMC to estimate the model.
+</p>
+
+<p>
+  This project implements Simplified Bayesian sensitivity analysis for
+  both continuous and binary variables, as described by
+  Gustafson <em>et al.</em>. The package can be installed
+  from <a href="http://cran.r-project.org/web/packages/SBSA/index.html">CRAN</a>.
+  To view the source repository and other project details, please
+  visit the <a href="http://<?php echo $domain; ?>/projects/<?php echo
+  $group_name; ?>/">project summary page</a> on R-Forge.
+</p>
+
+
+<h2>References</h2>
+<ol>
+<li><a name="Gustafson2009"/> Gustafson, P., McCandless, L.C., Levy,
+A.R., and Richardson, S. (2010). "Simplified Bayesian sensitivity
+analysis for mismeasured and unobserved confounders." Biometrics 66,
+1129-1137.
+(DOI: <a href="http://dx.doi.org/10.1111/j.1541-0420.2009.01377.x">10.1111/j.1541-0420.2009.01377.x</a>)
+</li>
+</ol>
 
 </body>
 </html>
